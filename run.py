d9bc118ab3c76c2a871adb0539d601eda8afbe15
ceciherriman <herrimanceci@gmail.com>
2026-06-10 14:38:02 -0400

    save .cache to github updates

--- run.py
+++ run.py
@@ -1,19 +1,12 @@
 import spotipy
-import os
 from spotipy.oauth2 import SpotifyOAuth
-from dotenv import load_dotenv
 
-load_dotenv()
-
-SPOTIPY_CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
-SPOTIPY_CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
-SPOTIPY_REDIRECT_URI = os.getenv("SPOTIPY_REDIRECT_URI")
+scope = "user-library-read playlist-modify-public playlist-modify-private"
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
 TOP_X = 50 #how many of the most recent liked songs to retrieve 
 
 ##get TOP_X most recent liked songs 
-scope = "user-library-read playlist-modify-public playlist-modify-private"
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI, scope=scope))
 likedSongs = sp.current_user_saved_tracks(limit=TOP_X)
 tracks = []
 
@@ -23,3 +16,5 @@ for song in likedSongs['items']:
 ##open Live Liked Songs playlist and add 
 livePlaylistId = '1t5rNLbkrzHlyH1z6fldmr'
 sp.playlist_replace_items(livePlaylistId, tracks)
+
+print("done updating!")
