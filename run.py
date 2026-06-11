c40deda66b34447f61e2e75c4abcfd579de0da73
ceciherriman <herrimanceci@gmail.com>
2026-06-10 22:30:20 -0400

    actions yaml token handling change

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
+
+load_dotenv()
 
 scope = "user-library-read playlist-modify-public playlist-modify-private"
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
